f263fb658b916f0c14c721c52667f5ec4d0b8362
gazelle.zaheer <gazelle.zaheer@gmail.com>
2019-05-01 19:24:18 +0200

    added routes for api with users routes and sign up route

---
+++ routes/api/users.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const router = require('express').Router();
+const auth = require('../auth');
+const Users = mongoose.model('Users');
+
+//POST new user route (optional, everyone has access)
+router.post('/', auth.optional, (req, res, next) => {
+    const { body: { user } } = req;
+  
+    if(!user.email) {
+      return res.status(422).json({
+        errors: {
+          email: 'is required',
+        },
+      });
+    }
+  
+    if(!user.password) {
+      return res.status(422).json({
+        errors: {
+          password: 'is required',
+        },
+      });
+    }
+    const finalUser = new Users(user);
+
+  finalUser.setPassword(user.password);
+
+  return finalUser.save()
+    .then(() => res.json({ user: finalUser.toAuthJSON() }));
+});
