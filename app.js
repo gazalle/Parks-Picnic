7369f9905020ec731a510500d67c0150418af826
gazelle.zaheer <gazelle.zaheer@gmail.com>
2019-05-01 19:28:27 +0200

    added login

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ mongoose.set('debug', true);
 //Models & routes
 require('./models/Users');
 require('./config/passport');
+app.use(require('./routes'));
 
 //Error handlers & middlewares
 if(!isProduction) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -30,3 +30,40 @@ router.post('/', auth.optional, (req, res, next) => {
   return finalUser.save()
     .then(() => res.json({ user: finalUser.toAuthJSON() }));
 });
+
+//POST login route (optional, everyone has access)
+router.post('/login', auth.optional, (req, res, next) => {
+    const { body: { user } } = req;
+  
+    if(!user.email) {
+      return res.status(422).json({
+        errors: {
+          email: 'is required',
+        },
+      });
+    }
+  
+    if(!user.password) {
+      return res.status(422).json({
+        errors: {
+          password: 'is required',
+        },
+      });
+    }
+    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+        if(err) {
+          return next(err);
+        }
+    
+        if(passportUser) {
+          const user = passportUser;
+          user.token = passportUser.generateJWT();
+    
+          return res.json({ user: user.toAuthJSON() });
+        }
+    
+        return status(400).info;
+      })(req, res, next);
+    });
+    
+    module.exports = router;
